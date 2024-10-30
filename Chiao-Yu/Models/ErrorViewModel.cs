83b4a86a12a4950a90811faf344d5e31842b2e51
Chiao Yu <chyu.inbox@gmial.com>
2024-10-30 11:55:04 +0100

    Add project files.

---
+++ Chiao-Yu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chiao_Yu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
